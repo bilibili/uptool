732256f8f0671a747bd46ba879c6cb40328a6b85
Xia Zhongyang <i@mechanus.io>
2018-05-28 16:36:59 +0800

    remove dummy comment in sidebar.vue

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -6,8 +6,6 @@
         <ul class="menu-list">
             <li> <router-link to="/submit">投稿</router-link> </li>
             <li> <router-link to="/placeholder">占位</router-link> </li>
-            <!-- <li><a>Dashboard</a></li>
-            <li><a>Customers</a></li> -->
         </ul>
     </aside>
 </template>
