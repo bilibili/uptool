2741e17384daaa83f7685d128e2a9b24ce0e4a52
Xia Zhongyang <i@mechanus.io>
2018-05-29 16:30:26 +0800

    add font-awesome as a dependency

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bulma/css/bulma.css'
+import 'font-awesome/css/font-awesome.css'
 // require("./js/ybuploader")
 
 Vue.config.productionTip = false
