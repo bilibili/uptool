487d1d53227393de264171764e6cebad197797dc
Xia Zhongyang <i@mechanus.io>
2018-05-29 14:23:58 +0800

    fix login.vue css z-index typo

--- src/components/login.vue
+++ src/components/login.vue
@@ -77,13 +77,13 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 iframe {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  zindex: 99999;
+  z-index: 99999;
 }
 </style>
