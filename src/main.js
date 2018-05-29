b868594d219e8afdb6e1444aa384e779c43e27a2
Xia Zhongyang <i@mechanus.io>
2018-05-29 16:43:07 +0800

    Revert "add font-awesome as a dependency"
    This reverts commit 2741e17384daaa83f7685d128e2a9b24ce0e4a52.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bulma/css/bulma.css'
-import 'font-awesome/css/font-awesome.css'
 // require("./js/ybuploader")
 
 Vue.config.productionTip = false
