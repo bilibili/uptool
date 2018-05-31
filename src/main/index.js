efbf359669c01ca6853053166ec028ab71b98931
Xia Zhongyang <i@mechanus.io>
2018-05-31 11:16:13 +0800

    disable window recreation

--- src/main/index.js
+++ src/main/index.js
@@ -140,12 +140,12 @@ app.on('window-all-closed', () => {
   }
 })
 
-app.on('activate', () => {
-  // on macOS it is common to re-create a window even after all windows have been closed
-  if (mainWindow === null) {
-    mainWindow = createMainWindow()
-  }
-})
+// app.on('activate', () => {
+//   // on macOS it is common to re-create a window even after all windows have been closed
+//   if (mainWindow === null) {
+//     mainWindow = createMainWindow()
+//   }
+// })
 
 // create main BrowserWindow when electron is ready
 app.on('ready', () => {
