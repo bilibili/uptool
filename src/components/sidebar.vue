7e1e089083e2c1f928fa84b50aa1c1356f3bee93
Xia Zhongyang <i@mechanus.io>
2018-06-01 16:45:47 +0800

    add placeholders

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -7,6 +7,12 @@
             <li> <router-link to="/submit">投稿</router-link> </li>
             <li><a @click="openManagementPage">稿件管理</a></li>
         </ul>
+        <p class="menu-label">
+            工具
+        </p>
+        <ul class="menu-list">
+            <li> <router-link to="/placeholder">转码</router-link> </li>
+        </ul>
     </aside>
 </template>
 
