79de970951d43b1a685ef68cdac0a3d55218e5ca
Xia Zhongyang <i@mechanus.io>
2018-06-01 20:29:11 +0800

    remove sidebar subtitle

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,16 +1,8 @@
 <template>
     <aside class="menu">
-        <p class="menu-label">
-            通用
-        </p>
         <ul class="menu-list">
             <li> <router-link to="/submit">投稿</router-link> </li>
             <li><a @click="openManagementPage">稿件管理</a></li>
-        </ul>
-        <p class="menu-label">
-            工具
-        </p>
-        <ul class="menu-list">
             <li> <router-link to="/placeholder">转码</router-link> </li>
         </ul>
     </aside>
