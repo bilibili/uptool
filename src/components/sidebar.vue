a9d92a32cffdafdd54a4c219a014439641a63b23
Xia Zhongyang <i@mechanus.io>
2018-06-01 16:39:27 +0800

    open mgt page in brower

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -5,7 +5,18 @@
         </p>
         <ul class="menu-list">
             <li> <router-link to="/submit">投稿</router-link> </li>
-            <li> <router-link to="/placeholder">占位</router-link> </li>
+            <li><a @click="openManagementPage">稿件管理</a></li>
         </ul>
     </aside>
 </template>
+
+<script>
+export default {
+    methods: {
+        openManagementPage: function () {
+            require('electron').shell.openExternal('https://member.bilibili.com/v2#/upload-manager/article')
+        }
+    }
+}
+</script>
+
