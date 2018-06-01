670e73a15f44cb2bcd339098f1096370e7478b26
Xia Zhongyang <i@mechanus.io>
2018-06-01 19:27:02 +0800

    input counter

---
+++ src/components/input_counter.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="input" :class="{'is-active': isFocused}">
+        <input :value="value"
+        @input="$emit('input', $event.target.value)"
+        @focus="isFocused=true"
+        @focusout="isFocused=false"
+        @keyup.enter="$emit('enter')"
+        :maxlength="maxlength"
+        :name="name"
+        :type="type"
+        class="input-instance">
+        <span class="is-size-7 has-text-grey">{{value.length}}/{{maxlength}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'input_counter',
+    props: [
+        'maxlength',
+        'name',
+        'type',
+        'value'
+    ],
+    data() {
+        return {
+            isFocused: false
+        }
+    }
+}
+</script>
+
+<style>
+.input-instance {
+  width: 100%;
+  height: 100%;
+  border: 0px none rgb(0,0,0);
+  font-size: inherit;
+}
+
+.input-instance:focus {
+  outline-width: 0;
+}
+</style>
