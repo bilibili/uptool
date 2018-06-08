51dae1879e917bf6d61471e17d06cba3c9541b0e
Xia Zhongyang <i@mechanus.io>
2018-06-08 17:08:03 +0800

    set vuex store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import 'bulma/css/bulma.css'
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
 import fontawesome from '@fortawesome/fontawesome';
@@ -12,5 +13,6 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+}
+
+const getters = {
+}
+
+const mutations = {
+}
+export default new Vuex.Store({
+    state,
+    getters,
+    mutations
+})
